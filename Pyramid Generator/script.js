35e8f65aa124df79c7e7e904c29c83a1eb940f51
Jomar0019 <jomarmagpoot24@gmail.com>
2024-06-14 17:38:26 +0800

    Pyramid Generator 100

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -1,6 +1,7 @@
-const character = "#";
-const count = 8;
+const character = "*";
+const count = 10;
 const rows = [];
+const inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return (
@@ -12,6 +13,26 @@ function padRow(rowNumber, rowCount) {
 
 // for (let i = 1; i <= count; i++) {
 //   rows.push(padRow(i, count));
+// }
+
+// UPSIDE DOWN USING UNSHIFT 
+for (let i = 1; i <= count; i++) {
+  if(inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+
+// while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }
+
+
+// UPSIDE DOWN PYRAMID 
+// for(let i = count; i > 0; i-- ) {
+//   rows.push(padRow(i, count))
 // }
 
 let result = "";
